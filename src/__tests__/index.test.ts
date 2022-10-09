cabcb45da948d8fe6a3d1aed08cb5146267e17b2
Ilya Sumbatyants <ilya.sumb@gmail.com>
2022-10-09 20:04:03 +0400

    Improve unit test coverage

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -17,51 +17,88 @@ const testData = Array(100)
   .map((_, i) => i);
 
 describe('transducers', () => {
-  it('transduce append works', () => {
-    const expected = transform(testData);
-    const result = tsd.transduce(xform, tsd.append, [] as string[])(testData);
+  describe('transduce', () => {
+    it('append works', () => {
+      const expected = transform(testData);
+      const result = tsd.transduce(xform, tsd.append, [] as string[])(testData);
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+
+    it('+ works', () => {
+      const xf = tsd.compose(
+        tsd.filter((x: number) => x % 2 !== 0),
+        tsd.map((x) => x * 2),
+        tsd.take(5)
+      );
+
+      const expected = testData
+        .filter((x) => x % 2 !== 0)
+        .map((x) => x * 2)
+        .slice(0, 5)
+        .reduce((r, x) => r + x, 0);
+      const result = tsd.transduce(xf, (r, x) => r + x, 0)(testData);
+
+      expect(result).toEqual(expected);
+    });
+
+    it('rethrows errors', () => {
+      const error = new Error('Not cool');
+      const xf = tsd.map((x: number) => {
+        throw error;
+      });
+
+      expect(() => {
+        tsd.transduce(xf, (r, x) => r + x, 0)(testData);
+      }).toThrowError(error);
+    });
   });
 
-  it('transduce + works', () => {
-    const xf = tsd.compose(
-      tsd.filter((x: number) => x % 2 !== 0),
-      tsd.map((x) => x * 2),
-      tsd.take(5)
-    );
+  describe('into', () => {
+    it('works', () => {
+      const expected = transform(testData);
+      const result = tsd.into([], xform)(testData);
 
-    const expected = testData
-      .filter((x) => x % 2 !== 0)
-      .map((x) => x * 2)
-      .slice(0, 5)
-      .reduce((r, x) => r + x, 0);
-    const result = tsd.transduce(xf, (r, x) => r + x, 0)(testData);
+      expect(result).toEqual(expected);
+    });
 
-    expect(result).toEqual(expected);
+    it('rethrows errors', () => {
+      const error = new Error('Not cool');
+      const xf = tsd.map((x: number) => {
+        throw error;
+      });
+
+      expect(() => tsd.into([], xf)(testData)).toThrowError(error);
+    });
   });
 
-  it('into works', () => {
-    const expected = transform(testData);
-    const result = tsd.into([], xform)(testData);
+  describe('sequence', () => {
+    it('works', () => {
+      const expected = transform(testData);
+      const result = [...tsd.sequence(xform, testData)];
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+
+    it('rethrows errors', () => {
+      const error = new Error('Not cool');
+      const xf = tsd.map((x: number) => {
+        throw error;
+      });
+
+      expect(() => [...tsd.sequence(xf, testData)]).toThrowError(error);
+    });
   });
 
-  it('take works multiple times', () => {
-    const expected = testData.slice(0, 5);
-    const xf = tsd.take<number>(5);
-    const result0 = tsd.into([], xf)(testData);
-    const result1 = tsd.into([], xf)(testData);
+  describe('specific xforms', () => {
+    it('take works multiple times', () => {
+      const expected = testData.slice(0, 5);
+      const xf = tsd.take<number>(5);
+      const result0 = tsd.into([], xf)(testData);
+      const result1 = tsd.into([], xf)(testData);
 
-    expect(result0).toEqual(expected);
-    expect(result1).toEqual(expected);
-  });
-
-  it('sequence works', () => {
-    const expected = transform(testData);
-    const result = [...tsd.sequence(xform, testData)];
-
-    expect(result).toEqual(expected);
+      expect(result0).toEqual(expected);
+      expect(result1).toEqual(expected);
+    });
   });
 });
